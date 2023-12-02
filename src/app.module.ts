c858ed74f207bd64aa68ced0dca190d7a5ed3b7f
lvy <upwardsrwr@gmail.com>
2023-12-02 15:49:20 +0800

    user

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserModule } from './user/user.module'
+import { User } from './user/entities/user.entity'
+import { Role } from './user/entities/role.entity'
+import { Permission } from './user/entities/permission.entity'
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'mysql',
+      synchronize: true,
+      logging: true,
+      entities: [User, Role, Permission],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password'
+      }
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService]
+})
+export class AppModule {}
---
+++ src/user/dto/auth.dto.ts
@@ -0,0 +1,11 @@
+export class RegisterUserDto {
+  username: string
+
+  nickName: string
+
+  password: string
+
+  email: string
+
+  captcha: string
+}
