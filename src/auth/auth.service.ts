cc0c384fb8a77e58f6ab284a5c45052467216387
lvy <upwardsrwr@gmail.com>
2023-12-30 22:26:46 +0800

    feat: init

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,89 +3,86 @@ import {
   HttpStatus,
   Inject,
   Injectable,
-  Logger
-} from '@nestjs/common'
-import { SignupDto } from './dto/signup.dto'
-import { RedisService } from 'src/redis/redis.service'
+  Logger,
+} from "@nestjs/common";
+import { SignupDto } from "./dto/signup.dto";
+import { RedisService } from "src/redis/redis.service";
 
-import { User } from 'src/user/entities/user.entity'
-import { Repository } from 'typeorm'
-import { InjectRepository } from '@nestjs/typeorm'
+import { User } from "src/user/entities/user.entity";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
 
-import { md5 } from 'src/utils'
-import { UserService } from 'src/user/user.service'
-import { LoginDto } from './dto/login.dto'
-import { LoginVo } from './vo/login.vo'
+import { md5 } from "src/utils";
+import { UserService } from "src/user/user.service";
+import { LoginDto } from "./dto/login.dto";
+import { LoginVo } from "./vo/login.vo";
 
-import { JwtService } from '@nestjs/jwt'
-import { ConfigService } from '@nestjs/config'
-import { AuthUserDto } from './dto/auth-user.dto'
-import { UpdateUserPasswordDto } from './dto'
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { AuthUserDto } from "./dto/auth-user.dto";
+import { UpdateUserPasswordDto } from "./dto";
 
 @Injectable()
 export class AuthService {
-  private logger = new Logger()
+  private logger = new Logger();
 
   @InjectRepository(User)
-  private userRepository: Repository<User>
+  private userRepository: Repository<User>;
 
   @Inject(RedisService)
-  private redisService: RedisService
+  private redisService: RedisService;
 
   @Inject(JwtService)
-  private jwtService: JwtService
+  private jwtService: JwtService;
 
   @Inject(ConfigService)
-  private configService: ConfigService
+  private configService: ConfigService;
 
   async signup(user: SignupDto) {
-    const captcha = await this.redisService.get(`captcha_${user.email}`)
-    console.log(captcha, 'captcha')
+    const captcha = await this.redisService.get(`captcha_${user.email}`);
+    console.log(captcha, "captcha");
     if (!captcha)
-      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST)
+      throw new HttpException("验证码已失效", HttpStatus.BAD_REQUEST);
 
     if (user.captcha !== captcha)
-      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST)
+      throw new HttpException("验证码不正确", HttpStatus.BAD_REQUEST);
 
     const findUser = await this.userRepository.findOneBy({
-      username: user.username
-    })
+      username: user.username,
+    });
 
-    if (findUser) throw new HttpException('用户已存在', HttpStatus.BAD_REQUEST)
+    if (findUser) throw new HttpException("用户已存在", HttpStatus.BAD_REQUEST);
 
-    const { username, password, email, nickName } = user
-    const newUser = new User()
-    newUser.username = username
-    newUser.password = md5(password)
-    newUser.email = email
-    newUser.nickName = nickName
+    const { username, password, email, nickName } = user;
+    const newUser = new User();
+    newUser.username = username;
+    newUser.password = md5(password);
+    newUser.email = email;
+    newUser.nickName = nickName;
 
     try {
-      await this.userRepository.save(newUser)
-      return '注册成功'
+      await this.userRepository.save(newUser);
+      return "注册成功";
     } catch (e) {
-      this.logger.error(e, UserService)
-      return '注册失败'
+      this.logger.error(e, UserService);
+      return "注册失败";
     }
   }
 
   async login(login: LoginDto) {
-    console.log(login, 'useruser')
     const user = await this.userRepository.findOne({
       where: {
-        username: login.username
+        username: login.username,
       },
-      relations: ['roles', 'roles.permissions']
-    })
+      relations: ["roles", "roles.permissions"],
+    });
 
-    console.log(user, 'useruseruseruser')
-
-    if (!user) throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST)
+    if (!user) throw new HttpException("用户不存在", HttpStatus.BAD_REQUEST);
 
     if (user.password !== md5(login.password))
-      throw new HttpException('密码错误', HttpStatus.BAD_REQUEST)
+      throw new HttpException("密码错误", HttpStatus.BAD_REQUEST);
 
-    const vo = new LoginVo()
+    const vo = new LoginVo();
 
     vo.userInfo = {
       id: user.id,
@@ -101,24 +98,24 @@ export class AuthService {
       permissions: user.roles.reduce((arr, item) => {
         item.permissions.forEach((permission) => {
           if (arr.indexOf(permission) === -1) {
-            arr.push(permission)
+            arr.push(permission);
           }
-        })
-        return arr
-      }, [])
-    }
+        });
+        return arr;
+      }, []),
+    };
 
-    return vo
+    return vo;
   }
 
   //获取登录的时候个人信息 权限认证
   async findUserById(userId: number) {
     const user = await this.userRepository.findOne({
       where: {
-        id: userId
+        id: userId,
       },
-      relations: ['roles', 'roles.permissions']
-    })
+      relations: ["roles", "roles.permissions"],
+    });
     return {
       id: user.id,
       username: user.username,
@@ -127,65 +124,65 @@ export class AuthService {
       permissions: user.roles.reduce((arr, item) => {
         item.permissions.forEach((permission) => {
           if (arr.indexOf(permission) === -1) {
-            arr.push(permission)
+            arr.push(permission);
           }
-        })
-        return arr
-      }, [])
-    }
+        });
+        return arr;
+      }, []),
+    };
   }
 
   // 生成 access token
   generateAccessToken(authUser: AuthUserDto) {
     return this.jwtService.sign(
       {
-        ...authUser
+        ...authUser,
       },
       {
         expiresIn:
-          this.configService.get('jwt_access_token_expires_time') || '30m'
-      }
-    )
+          this.configService.get("jwt_access_token_expires_time") || "30m",
+      },
+    );
   }
 
   // 生成 refresh token
   generateRefreshToken(userId: number) {
     return this.jwtService.sign(
       {
-        userId
+        userId,
       },
       {
         expiresIn:
-          this.configService.get('jwt_refresh_token_expres_time') || '7d'
-      }
-    )
+          this.configService.get("jwt_refresh_token_expres_time") || "7d",
+      },
+    );
   }
 
   async updatePassword(passwordDto: UpdateUserPasswordDto) {
     const captcha = await this.redisService.get(
-      `update_password_captcha_${passwordDto.email}`
-    )
+      `update_password_captcha_${passwordDto.email}`,
+    );
 
     if (!captcha) {
-      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST)
+      throw new HttpException("验证码已失效", HttpStatus.BAD_REQUEST);
     }
 
     const foundUser = await this.userRepository.findOneBy({
-      username: passwordDto.username
-    })
-    console.log(foundUser, 'foundUser')
+      username: passwordDto.username,
+    });
+    console.log(foundUser, "foundUser");
     if (foundUser.email !== passwordDto.email) {
-      throw new HttpException('邮箱不正确', HttpStatus.BAD_REQUEST)
+      throw new HttpException("邮箱不正确", HttpStatus.BAD_REQUEST);
     }
 
-    foundUser.password = md5(passwordDto.password)
+    foundUser.password = md5(passwordDto.password);
 
     try {
-      await this.userRepository.save(foundUser)
-      return '密码修改成功'
+      await this.userRepository.save(foundUser);
+      return "密码修改成功";
     } catch (e) {
-      this.logger.error(e, UserService)
-      return '密码修改失败'
+      this.logger.error(e, UserService);
+      return "密码修改失败";
     }
   }
 }
--- src/meeting-room/dto/create-meeting-room.dto.ts
+++ src/meeting-room/dto/create-meeting-room.dto.ts
@@ -1,27 +1,31 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, MaxLength } from 'class-validator'
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsNotEmpty, MaxLength } from "class-validator";
 
 export class CreateMeetingRoomDto {
   @ApiProperty()
-  @MaxLength(10, { message: '会议室名称最长为 10 字符' })
-  @IsNotEmpty({ message: '会议室名称不能为空' })
-  name: string
+  @MaxLength(10, { message: "会议室名称最长为 10 字符" })
+  @IsNotEmpty({ message: "会议室名称不能为空" })
+  name: string;
 
   @ApiProperty()
-  @IsNotEmpty({ message: '容量不能为空' })
-  capacity: number
+  @IsNotEmpty({ message: "容量不能为空" })
+  capacity: number;
 
   @ApiProperty()
-  @IsNotEmpty({ message: '位置不能为空' })
-  @MaxLength(50, { message: '位置最长为 50 字符' })
-  location: string
+  @IsNotEmpty({ message: "位置不能为空" })
+  @MaxLength(50, { message: "位置最长为 50 字符" })
+  location: string;
 
   @ApiProperty()
-  @MaxLength(50, { message: '设备最长为 50 字符' })
-  @IsNotEmpty({ message: '设备不能为空' })
-  equipment: string
+  @MaxLength(50, { message: "设备最长为 50 字符" })
+  @IsNotEmpty({ message: "设备不能为空" })
+  equipment: string;
 
   @ApiProperty()
-  @MaxLength(100, { message: '描述最长为 100 字符' })
-  description: string
+  @MaxLength(100, { message: "描述最长为 100 字符" })
+  description: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  isBooked: boolean;
 }
