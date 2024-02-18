8a6c643277abb9477f9d93bf0d3d2161938bc9d2
lvy <upwardsrwr@gmail.com>
2024-02-18 17:42:41 +0800

    fix: bug

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,180 +3,176 @@ import {
   Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
   HttpStatus,
   Inject,
   Query,
-  UnauthorizedException
-} from '@nestjs/common'
-import { AuthService } from './auth.service'
+  UnauthorizedException,
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
 import {
   ApiBody,
   ApiOperation,
   ApiQuery,
   ApiResponse,
-  ApiTags
-} from '@nestjs/swagger'
-import { EmailService } from 'src/email/email.service'
-import { RedisService } from 'src/redis/redis.service'
-import { JwtService } from '@nestjs/jwt'
-import { RefreshTokenVo } from './vo/refresh-token.vo'
-import { UpdateUserPasswordDto, LoginDto, SignupDto } from './dto'
-import { UserInfo } from './vo'
+  ApiTags,
+} from "@nestjs/swagger";
+import { EmailService } from "src/email/email.service";
+import { RedisService } from "src/redis/redis.service";
+import { JwtService } from "@nestjs/jwt";
+import { RefreshTokenVo } from "./vo/refresh-token.vo";
+import { UpdateUserPasswordDto, LoginDto, SignupDto } from "./dto";
 
-@ApiTags('权限')
-@Controller('auth')
+@ApiTags("权限")
+@Controller("auth")
 export class AuthController {
   @Inject(EmailService)
-  private emailService: EmailService
+  private emailService: EmailService;
 
   @Inject(RedisService)
-  private redisService: RedisService
+  private redisService: RedisService;
 
   @Inject(JwtService)
-  private jwtService: JwtService
+  private jwtService: JwtService;
 
   constructor(private readonly authService: AuthService) {}
 
-  @ApiOperation({ summary: '注册' })
+  @ApiOperation({ summary: "注册" })
   @ApiBody({ type: SignupDto })
   @ApiResponse({
     status: HttpStatus.BAD_REQUEST,
-    description: '验证码已失效/验证码不正确/用户已存在',
-    type: String
+    description: "验证码已失效/验证码不正确/用户已存在",
+    type: String,
   })
   @ApiResponse({
     status: HttpStatus.OK,
-    description: '注册成功/失败',
-    type: String
+    description: "注册成功/失败",
+    type: String,
   })
-  @Post('signup')
+  @Post("signup")
   async signup(@Body() signupData: SignupDto) {
-    return await this.authService.signup(signupData)
+    return await this.authService.signup(signupData);
   }
 
-  @ApiOperation({ summary: '登录' })
-  @Post('/login')
+  @ApiOperation({ summary: "登录" })
+  @Post("/login")
   async login(@Body() loginUser: LoginDto) {
-    const vo = await this.authService.login(loginUser)
-    vo.userInfo
+    const vo = await this.authService.login(loginUser);
+    vo.userInfo;
     vo.accessToken = this.authService.generateAccessToken({
       id: vo.userInfo.id,
       username: vo.userInfo.username,
       roles: vo.userInfo.roles,
       permissions: vo.userInfo.permissions,
-      isAdmin: vo.userInfo.isAdmin
-    })
-    vo.refreshToken = this.authService.generateRefreshToken(vo.userInfo.id)
-    return vo
+      isAdmin: vo.userInfo.isAdmin,
+    });
+    vo.refreshToken = this.authService.generateRefreshToken(vo.userInfo.id);
+    return vo;
   }
 
-  @ApiOperation({ summary: '获取注册邮箱验证码' })
+  @ApiOperation({ summary: "获取注册邮箱验证码" })
   @ApiQuery({
-    name: 'address',
+    name: "address",
     type: String,
-    description: '邮箱地址',
+    description: "邮箱地址",
     required: true,
-    example: 'xxx@xx.com'
+    example: "xxx@xx.com",
   })
   @ApiResponse({
     status: HttpStatus.OK,
-    description: '发送成功',
-    type: String
+    description: "发送成功",
+    type: String,
   })
-  @Get('register-captcha')
-  async captcha(@Query('address') address: string) {
-    const code = Math.random().toString().slice(2, 8)
+  @Get("register-captcha")
+  async captcha(@Query("address") address: string) {
+    const code = Math.random().toString().slice(2, 8);
 
-    const a = await this.redisService.set(`captcha_${address}`, code, 5 * 60)
+    await this.redisService.set(`captcha_${address}`, code, 5 * 60);
     await this.emailService.sendMail({
       to: address,
-      subject: '注册验证码',
-      html: `<p>你的注册验证码是 ${code}</p>`
-    })
-    return '发送成功'
+      subject: "注册验证码",
+      html: `<p>你的注册验证码是 ${code}</p>`,
+    });
+    return "发送成功";
   }
 
-  @ApiOperation({ summary: '刷新token' })
+  @ApiOperation({ summary: "刷新token" })
   @ApiQuery({
-    name: 'refreshToken',
+    name: "refreshToken",
     type: String,
-    description: '刷新 token',
+    description: "刷新 token",
     required: true,
-    example: 'refreshToken'
+    example: "refreshToken",
   })
   @ApiResponse({
     status: HttpStatus.UNAUTHORIZED,
-    description: 'token 已失效，请重新登录'
+    description: "token 已失效，请重新登录",
   })
   @ApiResponse({
     status: HttpStatus.OK,
-    description: '刷新成功',
-    type: RefreshTokenVo
+    description: "刷新成功",
+    type: RefreshTokenVo,
   })
-  @Get('refresh-token')
-  async refreshToken(@Query('refreshToken') refreshToken: string) {
+  @Get("refresh-token")
+  async refreshToken(@Query("refreshToken") refreshToken: string) {
     try {
-      const data = this.jwtService.verify(refreshToken)
+      const data = this.jwtService.verify(refreshToken);
 
-      const user = await this.authService.findUserById(data.userId)
+      const user = await this.authService.findUserById(data.userId);
 
-      const vo = new RefreshTokenVo()
+      const vo = new RefreshTokenVo();
 
       vo.access_token = this.authService.generateAccessToken({
         id: user.id,
         username: user.username,
         roles: user.roles,
         permissions: user.permissions,
-        isAdmin: user.isAdmin
-      })
-      vo.refresh_token = this.authService.generateRefreshToken(user.id)
+        isAdmin: user.isAdmin,
+      });
+      vo.refresh_token = this.authService.generateRefreshToken(user.id);
 
-      return vo
+      return vo;
     } catch (e) {
-      throw new UnauthorizedException('token 已失效，请重新登录')
+      throw new UnauthorizedException("token 已失效，请重新登录");
     }
   }
 
   // 发送邮箱验证码的接口
-  @ApiOperation({ summary: '更新密码邮箱验证码的接口' })
+  @ApiOperation({ summary: "更新密码邮箱验证码的接口" })
   @ApiQuery({
-    name: 'address',
-    description: '邮箱地址',
-    type: String
+    name: "address",
+    description: "邮箱地址",
+    type: String,
   })
   @ApiResponse({
     type: String,
-    description: '发送成功'
+    description: "发送成功",
   })
-  @Get('update_password/captcha')
-  async updatePasswordCaptcha(@Query('address') address: string) {
-    const code = Math.random().toString().slice(2, 8)
+  @Get("update_password/captcha")
+  async updatePasswordCaptcha(@Query("address") address: string) {
+    const code = Math.random().toString().slice(2, 8);
     await this.redisService.set(
       `update_password_captcha_${address}`,
       code,
-      10 * 60 * 60
-    )
+      10 * 60 * 60,
+    );
     await this.emailService.sendMail({
       to: address,
-      subject: '更改密码验证码',
-      html: `<p>你的更改密码验证码是 ${code}</p>`
-    })
-    return '发送成功'
+      subject: "更改密码验证码",
+      html: `<p>你的更改密码验证码是 ${code}</p>`,
+    });
+    return "发送成功";
   }
 
-  @ApiOperation({ summary: '更新密码' })
+  @ApiOperation({ summary: "更新密码" })
   @ApiBody({
-    type: UpdateUserPasswordDto
+    type: UpdateUserPasswordDto,
   })
   @ApiResponse({
     type: String,
-    description: '验证码已失效/不正确'
+    description: "验证码已失效/不正确",
   })
-  @Post(['update_password', 'admin/update_password'])
+  @Post(["update_password", "admin/update_password"])
   async updatePassword(@Body() passwordDto: UpdateUserPasswordDto) {
-    await this.authService.updatePassword(passwordDto)
+    await this.authService.updatePassword(passwordDto);
   }
 }
--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,31 +1,31 @@
-import { Global, Inject, Module } from '@nestjs/common'
-import { RedisService } from './redis.service'
-import { createClient } from 'redis'
-import { ConfigService } from '@nestjs/config'
+import { Global, Module } from "@nestjs/common";
+import { RedisService } from "./redis.service";
+import { createClient } from "redis";
+import { ConfigService } from "@nestjs/config";
 
 @Global()
 @Module({
   providers: [
     RedisService,
     {
-      provide: 'REDIS_CLIENT',
+      provide: "REDIS_CLIENT",
       async useFactory(configService: ConfigService) {
-        console.log(configService.get('redis_server_host'))
-        console.log(configService.get('redis_server_port'))
+        console.log(configService.get("redis_server_host"));
+        console.log(configService.get("redis_server_port"));
 
         const client = createClient({
           socket: {
-            host: configService.get('redis_server_host'),
-            port: configService.get('redis_server_port')
+            host: configService.get("redis_server_host"),
+            port: configService.get("redis_server_port"),
           },
-          database: configService.get('redis_server_db')
-        })
-        await client.connect()
-        return client
+          database: configService.get("redis_server_db"),
+        });
+        await client.connect();
+        return client;
       },
-      inject: [ConfigService]
-    }
+      inject: [ConfigService],
+    },
   ],
-  exports: [RedisService]
+  exports: [RedisService],
 })
 export class RedisModule {}
